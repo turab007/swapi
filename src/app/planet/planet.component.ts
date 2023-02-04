5e6a0c3ae9a14eb3c81f87ecd3692c5502885455
abbasi <abbasi@advaneo.de>
2023-02-04 14:49:19 +0100

    initial comit

---
+++ src/app/planet/planet.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-planet',
+  templateUrl: './planet.component.html',
+  styleUrls: ['./planet.component.css']
+})
+export class PlanetComponent {
+
+}
